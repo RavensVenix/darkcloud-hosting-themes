bacf0c408f6fa712701f7b7280193a89b15fe9a5
Cecilia Lucia Ximena <ravensvenix@gmail.com>
2023-02-20 20:02:49 +0800

    v1.8.0 Pterodactyl

--- resources/scripts/components/auth/LoginFormContainer.tsx
+++ resources/scripts/components/auth/LoginFormContainer.tsx
@@ -50,7 +50,7 @@ export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => 
             &copy; 2015 - {(new Date()).getFullYear()}&nbsp;
             <a
                 rel={'noopener nofollow noreferrer'}
-                href={'https://pterodactyl.io'}
+                href={'https://luciaximena.site/contact'}
                 target={'_blank'}
                 css={tw`no-underline text-neutral-500 hover:text-neutral-300`}
             >
